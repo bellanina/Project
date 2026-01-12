7bea6e9d273ca868b85c56b98ae8045dfb9dc894
bellanina <kozidoukonstantina@gmail.com>
2026-01-12 16:28:25 +0200

    Main logic developed

---
+++ src/main/java/com/example/CinemaApplication.java
@@ -0,0 +1,12 @@
+package com.example; // ΠΡΟΣΟΧΗ: Μόνο αυτό, τίποτα άλλο.
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaApplication.class, args);
+        System.out.println("Το Cinema Management System ξεκίνησε επιτυχώς!");
+    }
+}
